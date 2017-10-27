8f170d9b123dc748ea5e59c410900ef266f78422
dyozie <dyozie@pivotal.io>
2017-10-27 14:42:34 -0700

    updated configuration, redirects for deployment on greenplum.org; bundle update

--- gpdb-doc/book/redirects.rb
+++ gpdb-doc/book/redirects.rb
@@ -1,2 +1,2 @@
-r301 '/', '/docs/common/gpdb-features.html'
-r301 '/index.html', '/docs/common/gpdb-features.html'
+r301 '/', '/docs/500/common/gpdb-features.html'
+r301 '/index.html', '/docs/500/common/gpdb-features.html'
